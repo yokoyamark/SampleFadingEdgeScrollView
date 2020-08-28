3c0ad2dba5643870ea74cd344bf135402c363b75
yokoyamark <yokoyamark@users.noreply.github.com>
2020-08-28 23:40:15 +0900

    Initial Commit

---
+++ SampleFadingEdgeScrollView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleFadingEdgeScrollView
+//
+//  Created by 横山涼佑 on 2020/08/28.
+//  Copyright © 2020 yokoyamark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
